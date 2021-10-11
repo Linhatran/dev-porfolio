a94d0537791bf2993abe8b55afc3c9bdf1a9d733
Linh Tran <linhtanl51@gmail.com>
2021-10-11 11:04:37 -0700

    override default routes for portfolios#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # add alias for route and override default value
   get 'about', to: 'pages#about' # route name can be anything besides 'about', as long as we point to the 'about' method
   get 'contact', to: 'pages#contact'
 

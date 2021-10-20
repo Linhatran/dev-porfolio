2bd841b3202b033238c99d31e96805c71938f672
Linh Tran <linhtanl51@gmail.com>
2021-10-19 18:30:22 -0700

    add copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  before_action :set_copy_right
+
+  def set_copy_right
+    @copyright = DevViewTool::Renderer.copyright 'Linh Tran', 'All rights reserved'
+  end
+end
+
+module DevViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
